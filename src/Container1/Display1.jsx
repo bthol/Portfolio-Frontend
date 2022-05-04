80a2953ad5b1f8d8485d9b237b0b2345befaf82f
bthol <95544562+bthol@users.noreply.github.com>
2022-05-04 14:12:50 -0700

    make it better

---
+++ src/Container1/Display1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { useState } from "react";
+
+const Display1 = () => {
+    return (
+        <div>
+            <p>Display1 test pass</p>
+        </div>
+    )
+};
+
+export { Display1 };
