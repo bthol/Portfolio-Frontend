7f452da6fcb5177eb3ee1bac13b4d38fe507b565
bthol <95544562+bthol@users.noreply.github.com>
2022-07-29 20:00:42 -0700

    button elements + style + theme logic

--- src/Aside/Aside.jsx
+++ src/Aside/Aside.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import { ProfilePhoto } from '../Images/ProfilePhoto';
 
+//button logic
+const btnNormal = document.querySelector(`#btn-theme-normal`);
+btnNormal.addEventListener("click", setThemeNormal);
+function setThemeNormal() {
+  document.body.classList.remove("color-theme-dark");
+  document.body.classList.add("color-theme-normal");
+};
+const btnDark = document.querySelector(`#btn-theme-dark`);
+btnDark.addEventListener("click", setThemeDark);
+function setThemeDark() {
+  document.body.classList.remove("color-theme-normal");
+  document.body.classList.add("color-theme-dark");
+};
+
 const Aside = () => {
     return(
         <div>
@@ -10,6 +24,11 @@ const Aside = () => {
                 <a href="" className="link-desat" onClick={() => {alert("You wanted an error message?")}}>error message</a>
             </div>
             <p><b>About Me</b>: I am a Full Stack Web and App Developer searching for a position to utilize and further develop my creative and technological skillset.</p>
+            <br />
+            <div className="flex-between">
+                <button id="btn-theme-normal" className="buttons">Normal Theme</button>
+                <button id="btn-theme-dark" className="buttons dark-button">Dark Theme</button>
+            </div>
         </div>
     )
 }
