7bcb118cfffbd868e4b8585bdac17b2422c600c3
bthol <95544562+bthol@users.noreply.github.com>
2022-08-07 17:21:48 -0700

    list style

--- src/Content/Section4.jsx
+++ src/Content/Section4.jsx
@@ -7,25 +7,25 @@ const Section4 = () => {
             <div>
                 <p className="flex-between"><p className="text-large"><b>BitWise Industries</b></p>May 2022 - June 2022</p>
                 <ul>
-                    <p className="exp-list"><b>Description: </b>BitWise industry offers courses on computer programming and web development, offering a learning community to support its members in their journey to learning to code.</p>
+                    <li className="exp-list remove-bullet-list"><b>Description: </b>BitWise industry offers courses on computer programming and web development, offering a learning community to support its members in their journey to learning to code.</li>
                 </ul>
             </div>
             <div>
                 <p className="flex-between"><p className="text-large"><b>General Assembly</b></p>December 2021 - March 2022</p>
                 <ul>
-                    <p className="exp-list"><b>Description: </b>General Assembly is a full-stack software engineering coding bootcamp, which places students in an intensive, twelve-week course requiring 400+ hours of hands on experience with product development fundamentals, e.g. object-oriented programming, simple algorithms, front-end styling and layout technologies, responsive and interactive design, the MVC development model, the MERN stack, test-based development, web-application deployment, and team collaboration strategies.</p>
+                    <li className="exp-list remove-bullet-list"><b>Description: </b>General Assembly is a full-stack software engineering coding bootcamp, which places students in an intensive, twelve-week course requiring 400+ hours of hands on experience with product development fundamentals, e.g. object-oriented programming, simple algorithms, front-end styling and layout technologies, responsive and interactive design, the MVC development model, the MERN stack, test-based development, web-application deployment, and team collaboration strategies.</li>
                 </ul>
             </div>
             <div>
                 <p className="flex-between"><p className="text-large"><b>Berkeley City College</b></p>March 2021 - December 2021</p>
                 <ul>
-                    <p className="exp-list"><b>Subject of Study: </b>Biological Sciences</p>
+                    <li className="exp-list remove-bullet-list"><b>Subject of Study: </b>Biological Sciences</li>
                 </ul>
             </div>
             <div>
                 <p className="flex-between"><p className="text-large"><b>Diablo Valley College</b></p>June 2015 - May 2016</p>
                 <ul>
-                    <p className="exp-list"><b>Subject of Study: </b>Biological Sciences</p>
+                    <li className="exp-list remove-bullet-list"><b>Subject of Study: </b>Biological Sciences</li>
                 </ul>
             </div>
             <br />
