42720da6363fd544fc6a76158af93a9d15443eda
bthol <95544562+bthol@users.noreply.github.com>
2022-08-11 05:50:17 -0700

    removed joke alert

--- src/Aside/Aside.jsx
+++ src/Aside/Aside.jsx
@@ -2,16 +2,11 @@ import React from 'react';
 import { ProfilePhoto } from '../Images/ProfilePhoto';
 
 const Aside = (props) => {
-    const jokeAlert = (e) => {
-        e.preventDefault();
-        alert("You wanted an error message?");
-    }
     return(
         <div>
             <ProfilePhoto></ProfilePhoto>
             <div className="flex-between">
                 <a href="" rel="noreferrer" className="link-desat header-padd-right" onClick={props.featureAlert}>Contact</a>
-                <a href="" className="link-desat" onClick={jokeAlert}>error message</a>
             </div>
             <p><b>About Me</b>: I am a Full Stack Web and App Developer searching for a position to utilize and further grow my creative and technological skillset.</p>
             <br />
