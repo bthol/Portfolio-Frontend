a0bd6704dad7af8fedca9f228b619c9a52c61c40
bthol <95544562+bthol@users.noreply.github.com>
2022-09-20 09:39:26 -0700

    reverted to nav arrow problem start

--- src/NavButton/NavButton.jsx
+++ src/NavButton/NavButton.jsx
@@ -2,24 +2,24 @@ import { useState } from 'react';
 import { React } from 'react';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 
-class NavButton extends React.Component {
-    render() {
-        return <div>
+const NavButton = (props) => {
+    return (
+        <div>
             <button className="nav-buttons link-desat no-select"
                 onClick={(e) => {
                     e.preventDefault();
-                    this.props.closeNav();
-                    this.props.toggleNavState();
+                    props.closeNav();
+                    props.toggleNavState();
                 }}
-                >{this.props.name} <MdKeyboardArrowDown className={`nav-arrow ${this.props.drop}`} />
+                >{props.name} <MdKeyboardArrowDown className={`nav-arrow ${props.drop}`} />
             </button>
-            {this.props.navState &&
-                <div className={`nav-menu-style ${this.props.drop}`}>
-                    {this.props.links}
+            {props.navState &&
+                <div className={`nav-menu-style ${props.drop}`}>
+                    {props.links}
                 </div>
             }
         </div>
-    }
+    )
 }
 
 export { NavButton };
