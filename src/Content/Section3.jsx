fb7b64c4ddafb51d613b384a93e748845804affe
bthol <95544562+bthol@users.noreply.github.com>
2022-09-29 10:31:47 -0700

    fix tag decendant

--- src/Content/Section3.jsx
+++ src/Content/Section3.jsx
@@ -4,7 +4,7 @@ const Section3 = (props) => {
     return (
         <div>
             <div className="text-large" tabIndex={0}><b>{props.jobTitle}</b></div>
-            <p className="flex-between" tabIndex={0}><div className="text-large">{props.employer}</div>{props.workPeriod}</p>
+            <div className="flex-between" tabIndex={0}><p className="text-large">{props.employer}</p><p>{props.workPeriod}</p></div>
             {props.list}
         </div>
     )
