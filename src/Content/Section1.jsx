a8d21aa90e0dd1a239a8f50abd775b499147d699
bthol <95544562+bthol@users.noreply.github.com>
2022-10-27 10:37:38 -0700

    informational closure

--- src/Content/Section1.jsx
+++ src/Content/Section1.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Section1 = (props) => {
     return (
-        <div id={props.id}>
+        <div id={props.id} className="main-section-style-content">
             <p className="text-large" tabIndex={0}>{props.skillsTitle}</p>
             {props.skillsList}
         </div>
