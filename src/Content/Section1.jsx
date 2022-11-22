772b8de43ab0586ae11434b5f0d4245820a862a0
bthol <95544562+bthol@users.noreply.github.com>
2022-11-21 17:41:02 -0800

    hover effect for content on larger viewports

--- src/Content/Section1.jsx
+++ src/Content/Section1.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Section1 = (props) => {
     return (
-        <div id={props.id} className="main-section-style-content">
+        <div id={props.id} className="main-section-style-content hover-effect">
             <p className="text-large" tabIndex={0}>{props.skillsTitle}</p>
             {props.skillsList}
         </div>
