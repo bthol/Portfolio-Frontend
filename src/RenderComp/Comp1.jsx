fcbc49662a8d27302f889f2b5a9dd21d89c15dd2
bthol <95544562+bthol@users.noreply.github.com>
2023-03-16 09:18:02 -0700

    border spacing

--- src/RenderComp/Comp1.jsx
+++ src/RenderComp/Comp1.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Comp1 = (props) => {
     return (
         <div id={props.id} className="title-line">
-            <p className="text-large" tabIndex={0}>{props.skillsTitle}</p>
+            <div className="text-large" tabIndex={0}>{props.skillsTitle}</div>
             {props.skillsList}
         </div>
     )
