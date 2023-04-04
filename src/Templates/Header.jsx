7d74f7374834f69d28733d0ba74d7f98189d8d69
bthol <95544562+bthol@users.noreply.github.com>
2023-04-04 16:06:59 -0700

    tab navigation navbar

--- src/Templates/Header.jsx
+++ src/Templates/Header.jsx
@@ -5,36 +5,14 @@ import { NavButton } from './NavButton';
 
 const Header = (props) => {
 
-    const navData = [
-        {
-            name: "Pages ",
-            links: <div className="nav-menu-style">
-                <a href="" onClick={(e) => {
-                    e.preventDefault();
-                    props.goPage(0);
-                }} className="link-desat">Generic</a>
-            </div>,
-        },
-        {
-            name: "Apps ",
-            links: <div className="nav-menu-style">
-                <a href="https://bthol.github.io/Space-Battle/" target="_blank" className="link-desat">Space Battle</a>
-                <a href="https://bthol.github.io/Magic-8-Ball/" target="_blank" className="link-desat">Magic 8 Ball</a>
-                <a href="https://bthol.github.io/Retro-Toe/" target="_blank" className="link-desat">Retro Toe</a>
-                <a href="https://bthol.github.io/holiday-tree/" target="_blank" className="link-desat">Holiday Tree</a>
-                <a onClick={(e) => {
-                    props.featureAlertFunct(e);
-                }} href="" target="_blank" className="link-desat">Calculo</a>
-            </div>,
-        },
-    ];
-
     // NAV LOGIC
     // 1
     const [drop1, setDrop1] = useState("menu-close");
     const [navState1, setNavState1] = useState(false);
+    const [navTabIdx1, setNavTabIdx1] = useState(-1);
     const toggleNavState1 = () => {
         setNavState1(!navState1);
+        if (!navState1) {setNavTabIdx1(0)} else {setNavTabIdx1(-1)}
         menuDisplay1();
     }
     const menuDisplay1 = () => {
@@ -47,8 +25,10 @@ const Header = (props) => {
     // 2
     const [drop2, setDrop2] = useState("menu-close");
     const [navState2, setNavState2] = useState(false);
+    const [navTabIdx2, setNavTabIdx2] = useState(-1);
     const toggleNavState2 = () => {
         setNavState2(!navState2);
+        if (!navState2) {setNavTabIdx2(0)} else {setNavTabIdx2(-1)}
         menuDisplay2();
     }
     const menuDisplay2 = () => {
@@ -65,6 +45,31 @@ const Header = (props) => {
         setNavState2(false);
         setDrop2("menu-close");
     }
+
+    const navData = [
+        {
+            name: "Pages ",
+            links: <div className="nav-menu-style">
+                <a href="" onClick={(e) => {
+                    e.preventDefault();
+                    props.goPage(0);
+                }} className="link-desat" tabIndex={navTabIdx1}>Generic</a>
+            </div>,
+        },
+        {
+            name: "Apps ",
+            links: <div className="nav-menu-style">
+                <a href="https://bthol.github.io/Space-Battle/" target="_blank" className="link-desat" tabIndex={navTabIdx2}>Space Battle</a>
+                <a href="https://bthol.github.io/Magic-8-Ball/" target="_blank" className="link-desat" tabIndex={navTabIdx2}>Magic 8 Ball</a>
+                <a href="https://bthol.github.io/Retro-Toe/" target="_blank" className="link-desat" tabIndex={navTabIdx2}>Retro Toe</a>
+                <a href="https://bthol.github.io/holiday-tree/" target="_blank" className="link-desat" tabIndex={navTabIdx2}>Holiday Tree</a>
+                <a onClick={(e) => {
+                    props.featureAlertFunct(e);
+                }} href="" target="_blank" className="link-desat"  tabIndex={navTabIdx2}>Calculo</a>
+            </div>,
+        },
+    ];
+
     const siteTitleRef = useRef(null);
     // siteTitleRef.focus();
 
