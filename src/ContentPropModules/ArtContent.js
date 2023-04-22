a19bdb685a7da4b1ae85c8d7282733ed7ddc471b
bthol <95544562+bthol@users.noreply.github.com>
2023-04-22 12:31:22 -0700

    art page style + content object

--- src/ContentPropModules/ArtContent.js
+++ src/ContentPropModules/ArtContent.js
@@ -1,7 +1,32 @@
 const ArtContent = {
+    origami: [
+        {
+            image: <div>
+                <img src="" alt="img-origami-octopus" />
+            </div>,
+            name: <div>
+                <p><b>Octopus</b></p>
+            </div>,
+            describe: <div>
+                <p>The octopus isn't something you might of thought to make out of paper, because paper is rigid and octopuses are charactaristically flexible to the extant that they are nearly amorphous, but that just makes designing the model all the more interesting!</p>
+            </div>
+        },
+        {
+            image: <div>
+                <img src="" alt="origami-jellyfish" />
+            </div>,
+            name: <div>
+                <p><b>Jellyfish</b></p>
+            </div>,
+            describe: <div>
+                <p>The Jellyfish</p>
+            </div>
+
+        }
+    ],
     discussion: <div>
-        <p className="indent">I began creating art when I was a child, as many do, and from the very start I stood out. When asked to draw a tree, I made sure to include roots that submerged into the ground and resurfaced elsewhere and a top with branches upon branches with scraggly details in the leaves. I have always been enamored by details, and, contrary to many people who do art when they are younger, my interest in art hasn't waned a bit. In fact, I'd wager that I get more interested in art the older I get. As I find more techniques and more mediums to express the urge to create, I find more reasons to be creative.</p>
+        <p className="indent">I began creating art when I was a child, as many do, and from the very start I stood out. When asked to draw a tree, I made sure to include roots that submerged into the ground and resurfaced elsewhere and a top with branches upon branches with scraggly details in the leaves. I have always been enamored by details, and, contrary to many people who do art when they are younger, my interest in art hasn't waned a bit. In fact, I get more interested in art the older I get. As I find more techniques and more mediums to express the urge to create, I find more reasons to be creative.</p>
         <p className="indent"></p>
-    </div>
+    </div>,
 };
 export { ArtContent };
--- src/Pages/ArtPage.jsx
+++ src/Pages/ArtPage.jsx
@@ -4,11 +4,13 @@ import { ArtContent as Content } from '../ContentPropModules/ArtContent';
 const ArtPage = (props) => {
     return (
         <div id="artpage" className="page-content">
-            <div className="content-container shadow-behind container-first artpage-layout-width fx3d">
+
+            <div className="content-container container-first artpage-layout-width fx3d">
                 <div className="flex-center">
-                    <h3 className="underline text-x-large">Gallery</h3>
+                    <div className="text-x-large title-layout-pad">Gallery</div>
                 </div>
             </div>
+
             <div className="gallery-image gallery-image-column-1">
                 <img src="" alt="img1" />
             </div>
@@ -33,26 +35,30 @@ const ArtPage = (props) => {
             <div className="gallery-image gallery-image-column-4">
                 <img src="" alt="img4" />
             </div>
-            <div className="content-container artpage-layout-width shadow-behind">
+
+            <div className="content-container artpage-layout-width fx3d">
                 <div className="flex-center">
-                    <h3 className="underline text-x-large">Origami</h3>
+                    <div className="text-x-large title-layout-pad">Origami</div>
                 </div>
             </div>
+
             <div className="art-origami-model artpage-layout-width flex-center">
                 <div className="gallery-image">
-                    <img src="" alt="img-origami-1" />
+                    {Content.origami[0].image}
                 </div>
-                <div className="content-container fade-left">
-                    <p><b>Octopus</b></p>
-                    <p>The octopus isn't something you might of thought to make out of paper, because paper is rigid and octopuses are charactaristically flexible to the extant that they are nearly amorphous, but that just makes designing the model all the more interesting!</p>
+                <div className="content-container">
+                    {Content.origami[0].name}
+                    {Content.origami[0].describe}
                 </div>
             </div>
-            <div className="content-container artpage-layout-width shadow-behind ">
+
+            <div className="content-container artpage-layout-width fx3d">
                 <div className="flex-center">
-                    <h3 className="underline text-x-large">Discussion</h3>
+                    <div className="text-x-large title-layout-pad">Discussion</div>
                 </div>
             </div>
-            <div id="art-section-card-discussion" className="content-container shadow-behind container-last artpage-layout-width">
+
+            <div id="art-section-card-discussion" className="content-container container-last artpage-layout-width">
                 {Content.discussion}
             </div>
         </div>
