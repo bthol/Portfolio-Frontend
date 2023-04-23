63c106e6835881de22bc638c394159b673de3084
bthol <95544562+bthol@users.noreply.github.com>
2023-04-22 18:22:54 -0700

    improved artpage layout

--- src/ContentPropModules/ArtContent.js
+++ src/ContentPropModules/ArtContent.js
@@ -1,35 +1,51 @@
 const ArtContent = {
     origami: [
         {
-            image1: <div>
-                <img src="" alt="img-origami-octopus" />
-            </div>,
-            image2: <div>
-                <img src="" alt="img-origami-octopus-crease-pattern" />
-            </div>,
             name: <div>
                 <p><b>Octopus</b></p>
             </div>,
+            image1: <div>
+                <img className="img-origami" src="" alt="origami-octopus" />
+            </div>,
+            image2: <div>
+                <img className="img-origami" src="" alt="origami-octopus-crease-pattern" />
+            </div>,
             describe: <div>
-                <p>The octopus isn't something you might of thought to make out of paper, because paper is rigid and octopuses are charactaristically flexible to the extant that they are nearly amorphous, but that just makes designing the model all the more interesting! The technique which did the trick is called box-pleating, which is really efective at </p>
+                <p>The octopus isn't something you might have thought to make out of paper, because paper is rigid and octopuses are charactaristically flexible, but that just makes designing the model all the more interesting! The technique that did the trick is called box-pleating, which is really efective at narowing the flat of a section into an extremity for each of the eight arms of the octopus. Essentially, an octopus can be made out of a square by dividing the square into quadrants and box pleating each quadrant a tasteful number of times, where one center box and and its diagonal opposite are halved, so that the arms that end in the corners are equal in length to those that end at the midpoint of the paper's edge, and finally using the remaining two center boxes to make the eyes.</p>
             </div>
         },
         {
-            image: <div>
-                <img src="" alt="origami-jellyfish" />
-            </div>,
             name: <div>
-                <p><b>Jellyfish</b></p>
+                <p><b>Cuttlefish</b></p>
+            </div>,
+            image1: <div>
+                <img className="img-origami" src="" alt="origami-cuttlefish" />
+            </div>,
+            image2: <div>
+                <img src="" alt="origami-cuttlefish-crease-pattern" />
             </div>,
             describe: <div>
-                <p>The Jellyfish</p>
+                <p>Unlike the real deal, the cuttlefish model has tentacles of differing lengths, yet it meets the basic requirement for a cuttlefish: it has a body, two eyes, a cuttlefin, and the correct number of tentacles. This model was meant as more of conceptual model for cuttlefish than a paper exemplar of some particular species.</p>
             </div>
-
-        }
+        },
+        {
+            name: <div>
+                <p><b>Seahorse</b></p>
+            </div>,
+            image1: <div>
+                <img className="img-origami" src="" alt="origami-seahorse" />
+            </div>,
+            image2: <div>
+                <img className="img-origami" src="" alt="origami-seahorse-crease-pattern" />
+            </div>,
+            describe: <div>
+                <p>The seahorse model is one that is surprisingly simple due to the bilateral symmetry of seahorse morphology. It begins with the triangle base. What is nice about this model is that it is so simple that much of the character in each example of the model comes from subtler methods like shaping the head, pleating the fins to give them ridges, and angling the tail to give the seahorse some personality.</p>
+            </div>
+        },
     ],
     discussion: <div>
         <p className="indent">I began creating art when I was a child, as many do, and from the very start I stood out. When asked to draw a tree, I made sure to include roots that submerged into the ground and resurfaced elsewhere and a top with branches upon branches with scraggly details in the leaves. I have always been enamored by details, and, contrary to many people who do art when they are younger, my interest in art hasn't waned a bit. In fact, I get more interested in art the older I get. As I find more techniques and more mediums to express the urge to create, I find more reasons to be creative.</p>
-        <p className="indent"></p>
+        <p className="indent">bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla </p>
     </div>,
 };
 export { ArtContent };
--- src/Pages/ArtPage.jsx
+++ src/Pages/ArtPage.jsx
@@ -11,28 +11,28 @@ const ArtPage = (props) => {
                 </div>
             </div>
 
-            <div className="gallery-image gallery-image-column-1">
+            <div className="gallery-image gallery-image-column-1 hover-effect">
                 <img src="" alt="img1" />
             </div>
-            <div className="gallery-image gallery-image-column-2">
+            <div className="gallery-image gallery-image-column-2 hover-effect">
                 <img src="" alt="img2" />
             </div>
-            <div className="gallery-image gallery-image-column-3">
+            <div className="gallery-image gallery-image-column-3 hover-effect">
                 <img src="" alt="img3" />
             </div>
-            <div className="gallery-image gallery-image-column-4">
+            <div className="gallery-image gallery-image-column-4 hover-effect">
                 <img src="" alt="img4" />
             </div>
-            <div className="gallery-image gallery-image-column-1">
+            <div className="gallery-image gallery-image-column-1 hover-effect">
                 <img src="" alt="img1" />
             </div>
-            <div className="gallery-image gallery-image-column-2">
+            <div className="gallery-image gallery-image-column-2 hover-effect">
                 <img src="" alt="img2" />
             </div>
-            <div className="gallery-image gallery-image-column-3">
+            <div className="gallery-image gallery-image-column-3 hover-effect">
                 <img src="" alt="img3" />
             </div>
-            <div className="gallery-image gallery-image-column-4">
+            <div className="gallery-image gallery-image-column-4 hover-effect">
                 <img src="" alt="img4" />
             </div>
 
@@ -42,8 +42,8 @@ const ArtPage = (props) => {
                 </div>
             </div>
 
-            <div className="art-origami-model artpage-layout-width flex-center">
-                <div>
+            <div className="artpage-layout-width origami-grid-layout">
+                <div className="origami-grid-images">
                     {Content.origami[0].image1}
                     {Content.origami[0].image2}
                 </div>
@@ -53,13 +53,35 @@ const ArtPage = (props) => {
                 </div>
             </div>
 
+            <div className="artpage-layout-width origami-grid-layout">
+                <div>
+                    {Content.origami[1].image1}
+                    {Content.origami[1].image2}
+                </div>
+                <div className="content-container">
+                    {Content.origami[1].name}
+                    {Content.origami[1].describe}
+                </div>
+            </div>
+            
+            <div className="artpage-layout-width origami-grid-layout">
+                <div>
+                    {Content.origami[2].image1}
+                    {Content.origami[2].image2}
+                </div>
+                <div className="content-container">
+                    {Content.origami[2].name}
+                    {Content.origami[2].describe}
+                </div>
+            </div>
+
             <div className="content-container artpage-layout-width fx3d">
                 <div className="flex-center">
                     <div className="text-x-large title-layout-pad">Discussion</div>
                 </div>
             </div>
 
-            <div id="art-section-card-discussion" className="content-container container-last artpage-layout-width">
+            <div id="art-section-card-discussion" className="content-container container-last artpage-layout-width middle-text">
                 {Content.discussion}
             </div>
         </div>
