1d53b3f800108a4d2d68739384f5cbf1030254b4
bthol <95544562+bthol@users.noreply.github.com>
2023-07-08 01:13:58 -0700

    accordian arrows

--- src/RenderComp/Comp3.jsx
+++ src/RenderComp/Comp3.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 
 const Comp3 = (props) => {
     // SHOW MORE/LESS
-    const [showMSG, setShowMSG] = useState("more");
     const [show, setShow] = useState(false);
     const [shown, setShown] = useState("down-arrow");
     const [togshow, setTogshow] = useState(false);
@@ -11,11 +10,9 @@ const Comp3 = (props) => {
         setTogshow(!togshow);
         if (!togshow) {
         setShow(true);
-        setShowMSG("less");
         setShown("up-arrow");
         } else {
         setShow(false);
-        setShowMSG("more");
         setShown("down-arrow");
         }
     }
@@ -34,7 +31,7 @@ const Comp3 = (props) => {
                 if (props.enter(e)) {
                     toggleShown();
                 }
-            }}><div className="flex-center button-hover"><p className="cursor-pointer">{showMSG}</p><p className={`nav-arrow ${shown}`}></p></div></div>
+            }}><div className="flex-center button-hover"><p className={`arrow ${shown}`}></p></div></div>
             <hr noshade="true" />
             <br />
         </div>
