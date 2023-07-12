20c0dbf94cfeacacc2256ef10e890a3e82cb2c1c
bthol <95544562+bthol@users.noreply.github.com>
2023-07-11 20:44:01 -0700

    modal style

--- src/Modals/Timeout.jsx
+++ src/Modals/Timeout.jsx
@@ -4,13 +4,17 @@ const Timeout = (props) => {
     return (
         <div className={`modal modal-container flex-center`}>
             <div className={`modal modal-content shadow-behind`}>
-            <h1>Inactive</h1>
-            <hr />
-            <p className="text-xlarge">Are you still there?</p>
-            <br />
-            <div>
-                <button onClick={() => {props.setModal(false)}} className="buttons modal-button" autoFocus>Yes, I'm still here</button>
-            </div>
+                <div>
+                    <h1>Inactive</h1>
+                    <hr />
+                </div>
+                <div>
+                    <p className="text-xlarge">Are you still there?</p>
+                    <br />
+                    <div>
+                        <button onClick={() => {props.setModal(false)}} className="buttons modal-button" autoFocus>Yes, I'm still here</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
