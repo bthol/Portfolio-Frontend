397e9827927ddd38c871725ed5fb84df5b50c6d1
bthol <95544562+bthol@users.noreply.github.com>
2023-08-09 12:51:10 -0700

    image resizes based on area

--- src/RenderComp/Comp7.jsx
+++ src/RenderComp/Comp7.jsx
@@ -1,24 +1,75 @@
 import React, { useState } from "react";
 
 const Comp7 = (props) => {
+    const [sized, setSized] = useState(false);
     const [select, setSelect] = useState(true);
     const toggleSelect = (e) => {
-        // get height and width of Area
-        // get height and width of Image
-        // calculate aspect ratios (AR)
-        // where AR of Area < AR of Image
-            // (Height of Area / Height of Image) * intial Width of Image = final Width of Image
-            // assign final Width of Image to image
-        // otherwise
-            // (Smaller dimension of Area / AR of Image) * intial Width of Image = final Width of Image
-            // assign final Width of Image to image
-        // Rerender
-        setSelect(!select);
+        new Promise((resolve, err) => {
+            setSelect(!select);
+            resolve("resolved")
+        }).then(() => {
+            if (select === true) {
+                // select the area
+                const a = document.getElementsByClassName("gallery-image-select")[0];
+                // get height and width of Area
+                let aH = a.offsetHeight;
+                let aW = a.offsetWidth;
+                // select the image
+                const i = e.target;
+                // get height and width of Image
+                const iH = i.offsetHeight;
+                const iW = i.offsetWidth;
+                // calculate aspect ratios (AR)
+                let aAR = aH / aW;
+                const iAR = iH / iW;
+                // where AR of Area < AR of Image
+                    // (Height of Area / Height of Image) * intial Width of Image = final Width of Image
+                    // assign final Width of Image to image
+                // otherwise
+                    // (Smaller dimension of Area / AR of Image) * intial Width of Image = final Width of Image
+                    // assign final Width of Image to image
+                const getNewSize = (aAR, iAR, aH, aW, iH, iW) => {
+                    let newSize;
+                    if (aAR < iAR) {
+                        console.log(1);
+                        newSize = (aH / iH) * iW;
+                    } else {
+                        if (aH > aW) {
+                            console.log(2);
+                            newSize = aW / iAR;
+                        } else {
+                            console.log(3);
+                            newSize = (aW - iW) + iW;
+                        }
+                    }
+                    return newSize;
+                };
+                new Promise((resolve) => {
+                    setSized(true);
+                    resolve("resolved")
+                }).then(() => {
+                    const newSize = getNewSize(aAR, iAR, aH, aW, iH, iW);
+                    a.firstChild.style.width = `${newSize - 55}px`;
+                })
+                document.addEventListener("resize", () => {
+                    console.log("ran");
+                    aH = a.offsetHeight;
+                    aW = a.offsetWidth;
+                    aAR = aH / aW;
+                    const newSize = getNewSize(aAR, iAR, aH, aW, iH, iW);
+                    a.firstChild.style.width = `${newSize -100}px`;
+                })
+            }
+        })
     }
 
     return(
         <div className={`gallery-image-container ${props.galleryColumn}`} onClick={(e) => {toggleSelect(e)}} onKeyDown={(e) => {if (props.enter(e)) {toggleSelect(e)}}}>
-            {!select && <div className="gallery-image-select">{props.image}</div>}
+            {
+                !select && <div className="gallery-image-select">
+                    {sized && props.image}
+                </div>
+            }
             <div>{props.image}</div>
         </div>
     )
