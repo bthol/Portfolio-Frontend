e56e2cfad2435733ff418e7c4d17988149e63932
bthol <95544562+bthol@users.noreply.github.com>
2023-08-20 12:39:45 -0700

    general popup

--- src/RenderComp/Comp9.jsx
+++ src/RenderComp/Comp9.jsx
@@ -3,26 +3,18 @@ import { useState } from "react";
 
 const Comp9 = (props) => {
     const [show, setShow] = useState(false);
-    const [style, setStyle] = useState("");
     const toggle = () => {
         setShow(!show)
-        if (show) {
-            setStyle("")
-        } else {
-            setStyle("expand-text")
-        }
     }
     return(
-        <div className={`flex-center ${style}`} onClick={toggle}>
-            <div className="content-highlight">
-                <div className="flex-center link-desat"><b>{props.title}</b></div>
-                { show &&
-                    <div className="cursor-default">
-                        <br />
-                        {props.text}
-                    </div>
-                }
-            </div>
+        <div className="content-highlight" onClick={toggle}>
+            <div className="flex-center link-desat"><b>{props.title}</b></div>
+            { show &&
+                <div className="cursor-default">
+                    <br />
+                    {props.text}
+                </div>
+            }
         </div>
     )
 }
